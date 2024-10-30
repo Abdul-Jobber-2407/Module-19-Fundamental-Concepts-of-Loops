14ae1337e86047f64ceee1334431e887314b3ef7
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-30 10:52:30 +0600

    added to a final push

---
+++ case.js
@@ -0,0 +1,12 @@
+const school = ' Gokulkhali High school';
+// console.log(school);
+
+const subject = 'Chemistry';
+const book = 'chemistry' ;
+
+if(subject === book){
+    console.log(' I am reading book aibar porikkhai fatai felmu');
+}
+else{
+    console.log:('hudai hudai prista ulai ar vat khai ');
+}
